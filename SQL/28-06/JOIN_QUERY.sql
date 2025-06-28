6f4ee2b1ebc8a79f3632046e7be8c09d83575a19
Oyewusi Samuel Oluwatobi <41025327+suntwister@users.noreply.github.com>
2025-06-28 12:19:03 +0100

    Update JOIN_QUERY.sql

--- SQL/28-06/JOIN_QUERY.sql
+++ SQL/28-06/JOIN_QUERY.sql
@@ -19,5 +19,23 @@ RENAME COLUMN name TO customer_name;
 SELECT c.customer_name, o.order_id, o.order_amount
 FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id;
+
 -- List all orders and their corresponding customer names (using INNER JOIN)
-SELECT 
+SELECT o.order_id, c.customer_name, o.order_amount
+FROM orders o
+INNER JOIN customers c ON o.customer_id = c.customer_id;
+
+--Find customers who never placed an order (using LEFT JOIN and WHERE)
+SELECT c.customer_name
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
+
+--  Summary table
+SELECT c.customer_name,
+       COALESCE(SUM(o.order_amount), 0) AS total_amount,
+       COUNT(o.order_id) AS num_orders
+FROM customers c  
+LEFT JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_name;
