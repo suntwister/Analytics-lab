6df454556886eb35ffbac2da05d0c73d8a16878b
Oyewusi Samuel Oluwatobi <41025327+suntwister@users.noreply.github.com>
2025-07-23 16:47:15 +0100

    Update DB_queries.sql

--- SQL/23-07/DB_queries.sql
+++ SQL/23-07/DB_queries.sql
@@ -10,3 +10,14 @@ FROM vehicles v
 JOIN fuel_logs f ON v.vehicle_id = f.vehicle_id
 GROUP BY v.region;
 
+-- 2 Average deliveries per assignment per region
+SELECT * FROM vehicles;
+SELECT * FROM daily_consignments;
+SELECT 
+	v.region AS Region,
+    AVG(d.deliveries) AS Avg_deliveries
+FROM Vehicles v
+JOIN daily_consignments d ON v.vehicle_id = d.vehicle_id
+GROUP BY v.region;
+
+-- 3 
