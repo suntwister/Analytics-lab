1257f27829fe86912f4de63a9b60cba91539cb04
Oyewusi Samuel Oluwatobi <41025327+suntwister@users.noreply.github.com>
2025-07-23 17:04:18 +0100

    Update DB_queries.sql

--- SQL/23-07/DB_queries.sql
+++ SQL/23-07/DB_queries.sql
@@ -11,8 +11,6 @@ JOIN fuel_logs f ON v.vehicle_id = f.vehicle_id
 GROUP BY v.region;
 
 -- 2 Average deliveries per assignment per region
-SELECT * FROM vehicles;
-SELECT * FROM daily_consignments;
 SELECT 
 	v.region AS Region,
     AVG(d.deliveries) AS Avg_deliveries
@@ -20,4 +18,18 @@ FROM Vehicles v
 JOIN daily_consignments d ON v.vehicle_id = d.vehicle_id
 GROUP BY v.region;
 
--- 3 
+-- 3 Top 3 most fuel-consuming vehicles
+SELECT 
+	v.vehicle_id,
+    v.plate_number,
+    v.model,
+    SUM(f.liters_consumed) AS Total_fuel
+FROM vehicles v 
+JOIN fuel_logs f ON v.vehicle_id = f.vehicle_id
+GROUP BY v.vehicle_id, v.plate_number
+ORDER BY Total_fuel DESC
+LIMIT 3;    
+
+-- 4 Daily delivery summary (for the last 7 days)
+SELECT * FROM vehicles;
+SELECT * FROM fuel_logs;
