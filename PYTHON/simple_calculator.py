b6590abcdfdc4674ea96edb68f192bee7219a6fb
Samuel Oyewusi <s.suntwister@gmail.com>
2025-09-02 18:53:43 +0100

    Ammended calc project

--- PYTHON/simple_calculator.py
+++ PYTHON/simple_calculator.py
@@ -1,33 +1,39 @@
 # simple calculator program
 
-while True:
-    try:
-        # Get first number 
-        number_1 = int(input("Enter the first number: "))
-        # Get operator
-        operator = input("Enter the operstion you want to perform i.e * + - /: ")
-        # Get second number
-        number_2 = int(input("Enter the second number: "))
+def calculate(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            return num1 / num2
+        return "Division by zero not valid"
+    else:
+        return "Invalid Operator selection"
 
-        # opersations using control floe
-        if operator == "+":
-            result = number_1 + number_2
-        elif operator == "-":
-            result = number_1 - number_2
-        elif operator == "*":
-            result = number_1 * number_2
-        elif operator == "/":
-            if number_2 == 0:
-                print("Division by zero not allowed")
-                continue
-            result = number_1 / number_2
-        else:
-            print("Invalid operator, use (+ or - or / or *)")
-            continue 
-        print(f"Result: {result}")
-    except ValueError:
-        print("Invalid input. Enter a valid number")
-    choice = input("Would you like to perform another calculaton (Yes/No): ").title()
-    if choice == "No":
-        print("closing calculator")
-        break
+def main():
+    while True:
+        try:
+            # Get first number 
+            num1 = float(input("Enter the first number: "))
+            # Get operator
+            operator = input("Enter the operstion you want to perform i.e * + - /: ")
+            # Get second number
+            num2 = float(input("Enter the second number: "))
+
+            result = calculate(num1,num2,operator)
+            print(f"Result: {result}")
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+
+        # program exit
+        choice = input("Would you like to perform another calculaton (yes/no): ").strip().lower()
+        if choice == "no":
+            print("closing calculator")
+            break
+
+if __name__ == "__main__":
+    main()
